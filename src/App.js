939594c85db02072cfaca209bbb2b3a81e70c194
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-28 11:29:22 -0400

    add Authenticator Comp for listening auth events with Hub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react";
+import { Auth, Hub } from "aws-amplify";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    // console.dir(AmplifyTheme);
+    this.getUserData();
+    Hub.listen("auth", this, "onHubCapsule");
+  }
+
+  getUserData = async () => {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      user ? this.setState({ user }) : this.setState({ user: null });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("aum namah shivaya signed in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("aum namah shivaya signedup");
+        break;
+      case "signOut":
+        console.log("aum namah shivaya signout");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+
+    return !user ? <Authenticator theme={theme} /> : <div>App</div>;
   }
 }
 
@@ -30,4 +68,6 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+
+export default App;
