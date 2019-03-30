c4a66850a464b2ef9d9bf1a0e7d309e1ffe673ff
shambho <Sivakrishna.Karasala@eku.edu>
2019-03-30 19:29:05 -0400

    add lambdafunction for stripe checkout

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { API } from "aws-amplify";
 import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
 
 const stripeConfig = {
   currency: "USD",
-  publishableAPIKey: process.env.REACT_APP_STRIPE_API
+  publishableAPIKey: process.env.REACT_APP_STRIPE_KEY
 };
 const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    try {
+      const result = await API.post("orderlambda", "/charge", {
+        body: {
+          token
+        }
+      });
+      console.log({ result });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <StripeCheckout
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
